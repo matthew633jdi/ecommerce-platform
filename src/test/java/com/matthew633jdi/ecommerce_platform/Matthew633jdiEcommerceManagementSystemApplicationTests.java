2c521a91d4c0c91b11db8a9aa59a92970fe11d85
matthew633jdi <matthew633jdi@gmail.com>
2025-06-07 00:57:03 +0900

    Initial commit of project

---
+++ src/test/java/com/matthew633jdi/ecommerce_platform/Matthew633jdiEcommerceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthew633jdi.ecommerce_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Matthew633jdiEcommerceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
